bb2577964b6f89837803c4ad1213e071c112544d
corey szopinski <corey@core-industries.com>
2017-10-31 12:01:53 -0600

    updating to nvidia-supplied images and adding attach script

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-nvidia-docker run -it --rm --name digits -v /media/corey/raid/projects:/projects coreindustries/digits
+#nvidia-docker run -it --rm --name digits -v /media/corey/raid/projects:/projects coreindustries/digits
+nvidia-docker run --name digits -d -p 5000:5000 -p 6006:6006 -v /media/corey/raid/projects/digits:/jobs nvidia/digits:6.0
